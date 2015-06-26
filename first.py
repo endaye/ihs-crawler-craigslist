416ca866aa8592f995d6531d6a3deaa8be0bccc1
Yuancheng Zhang <old.far.hot@gmail.com>
2015-06-26 04:17:22 -0500

    demo code

--- first.py
+++ first.py
@@ -1,1 +1,6 @@
-print("I love tuna!")
+classmates = {'Tony': 'cool but smells', 'Emma': 'sits behind me', 'Lucy': 'asks too many questions'}
+print(classmates)
+print(classmates['Emma'])
+
+for key, value in classmates.items():
+    print(key, value)
