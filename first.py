26d9737bffab8a7542e59c851ca1b5279f494635
Yuancheng Zhang <old.far.hot@gmail.com>
2015-06-28 20:23:49 -0500

    correct test

--- first.py
+++
@@ -1,6 +0,0 @@
-classmates = {'Tony': 'cool but smells', 'Emma': 'sits behind me', 'Lucy': 'asks too many questions'}
-print(classmates)
-print(classmates['Emma'])
-
-for key, value in classmates.items():
-    print(key, value)
