502c41fced2f65962f928a3e21265766fdc61089
Yuancheng Zhang <old.far.hot@gmail.com>
2015-06-28 23:41:38 -0500

    29. Classes and Objects

---
+++ 29_class_obj.py
@@ -0,0 +1,20 @@
+class Enemy:  # 大写
+    life = 3
+
+    def attack(self):
+        print('ouchi')
+        self.life -= 1
+
+    def checkLife(self):
+        if self.life <= 0:
+            print("I am dead")
+        else:
+            print(str(self.life) + " life left")
+
+enemy1 = Enemy()
+enemy2 = Enemy()
+
+enemy1.attack()
+enemy1.attack()
+enemy1.checkLife()
+enemy2.checkLife()
