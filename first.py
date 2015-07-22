ac1ca1cfb7c1cd4007b3931a8418580074c2f513
vmvc2v <old.far.hot@gmail.com>
2015-07-22 14:45:46 -0500

    update crawler

--- first.py
+++ first.py
@@ -1,1 +1,13 @@
-print("I love tuna!")
+import threading
+
+
+class Messenger(threading.Thread):
+    def run(self):
+        for _ in range(10):
+            print(threading.currentThread().getName())
+
+
+x = Messenger(name='Send out messages')
+y = Messenger(name='Receive messages')
+x.start()
+y.start()
